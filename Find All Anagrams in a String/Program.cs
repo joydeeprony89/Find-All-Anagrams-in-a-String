e4e1b19749c533b825e3a069f5f8c313ef87d5f0
Jaydeep Shil <ED8A@eurofins.com>
2020-11-22 22:37:05 +0530

    Add project files.

---
+++ Find All Anagrams in a String/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Find_All_Anagrams_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = FindAnagrams("cbaebabacd", "abc");
+            Console.WriteLine(string.Join("-", result));
+        }
+
+        static IList<int> FindAnagrams(string s, string p)
+        {
+            IList<int> result = new List<int>();
+            int[] hash = new int[256];
+            foreach (char c in p.ToCharArray())
+                hash[(int)c]++;
+
+            int left = 0;
+            int right = 0;
+            int count = 0;
+            while(right < s.Length)
+            {
+                if(hash[(int)s[right]] > 0)
+                {
+                    hash[(int)s[right]]--;
+                    count++;
+                    right++;
+                }
+                else
+                {
+                    hash[(int)s[left]]++;
+                    count--;
+                    left++;
+                }
+
+                if(count == p.Length) result.Add(left);
+            }
+            return result;
+        }
+    }
+}
