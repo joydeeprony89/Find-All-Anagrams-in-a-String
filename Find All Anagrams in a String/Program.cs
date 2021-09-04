6dfd10fea234640732ac7a3ffa9101d0ebdda173
Jaydeep Shil <ED8A@eurofins.com>
2021-09-04 08:39:34 +0530

    new code added for interger array

--- Find All Anagrams in a String/Program.cs
+++ Find All Anagrams in a String/Program.cs
@@ -9,13 +9,17 @@ namespace Find_All_Anagrams_in_a_String
         {
             var result = FindAnagrams("cbaebabacd", "abc");
             Console.WriteLine(string.Join("-", result));
+            var products = new int[] { 3, 2, 1, 5, 2, 1, 2, 1, 3, 4 };
+            var candidate = new int[] { 1, 2, 3 };
+            var anotherResult = FindAnagramsFromIntegerArray(products, candidate);
+            Console.WriteLine(string.Join("-", anotherResult));
         }
 
         static IList<int> FindAnagrams(string s, string p)
         {
             IList<int> result = new List<int>();
             int[] hash = new int[256];
-            foreach (char c in p.ToCharArray())
+            foreach (char c in p)
                 hash[(int)c]++;
 
             int left = 0;
@@ -24,13 +28,13 @@ namespace Find_All_Anagrams_in_a_String
             while(right < s.Length)
             {
                 if(hash[(int)s[right]] > 0)
-                {
+                 {
                     hash[(int)s[right]]--;
                     count++;
                     right++;
                 }
                 else
-                {
+                 {
                     hash[(int)s[left]]++;
                     count--;
                     left++;
@@ -38,6 +42,42 @@ namespace Find_All_Anagrams_in_a_String
 
                 if(count == p.Length) result.Add(left);
             }
+            return result;
+        }
+
+        static IList<int> FindAnagramsFromIntegerArray(int[] product, int[] candidate)
+        {
+            Dictionary<int, int> hash = new Dictionary<int, int>();
+            IList<int> result = new List<int>();
+            foreach (int i in candidate)
+                if (!hash.ContainsKey(i)) hash.Add(i, 1);
+                else hash[i] += 1;
+
+            int l = 0, r = 0, count = 0;
+            while (r < product.Length)
+            {
+                if (hash.ContainsKey(product[r]) && hash[product[r]] > 0)
+                {
+                    hash[product[r]]--;
+                    count++;
+                    r++;
+                }
+                else
+                {
+                    if (!hash.ContainsKey(product[l]))
+                    {
+                        hash.Add(product[l], 1);
+                    }
+                    else
+                    {
+                        hash[product[l]]++;
+                    }
+                    count--;
+                    l++;
+                }
+                if (count == candidate.Length) result.Add(l);
+            }
+
             return result;
         }
     }
